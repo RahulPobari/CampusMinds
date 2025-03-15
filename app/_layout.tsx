8b4302a3bcd43966091f3c028e13a297e65a514f
RahulPobari <pobarirahul134@gmail.com>
2025-03-15 15:10:09 +0530

    code updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,10 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function RootLayout() {
   return (
     <>
-      {/* Ensure StatusBar is initialized first */}
+     
       <StatusBar style="dark" hidden={false} translucent={true} backgroundColor="transparent" />
 
-      {/* SafeAreaView ensures content is placed correctly below the StatusBar */}
+     
       <SafeAreaView style={{ flex: 1 }}>
         <Stack>
           <Stack.Screen
