e2f93f1cd6bcef6fab318f36f73c98aed40eef7c
RahulPobari <pobarirahul134@gmail.com>
2025-03-17 14:22:38 +0530

    code updated

--- configs/NilePostgresConfig.tsx
+++ configs/NilePostgresConfig.tsx
@@ -2,10 +2,10 @@ import { Client } from 'node-postgres'
 
 
 export const client = new Client({
-    user: process.env.EXPO_PUBLIC_DB_USERNAME,
-    password: process.env.EXPO_PUBLIC_DB_PASSWORD,
+    user: "0195a341-72fe-711a-9951-e8e12af2e613",
+    password: "5f9226c1-43fb-4d70-b562-454f07d9d6d2",
     host: "us-west-2.db.thenile.dev",
     port: 5432,
-    database: "CampusMinds",
-
+    database: "project",
 });
+
