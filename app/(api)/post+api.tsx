c15d54334436ad58fc85fc47ba19a92e7125221c
RahulPobari <pobarirahul134@gmail.com>
2025-03-18 21:31:38 +0530

    code updated

--- app/(api)/post+api.tsx
+++ app/(api)/post+api.tsx
@@ -4,9 +4,7 @@ export async function POST(request: Request) {
     const { content, imageUrl, visibleIn, email } = await request.json();
 
     await client.connect();
-
     const result = await client.query(`insert into post values(DEFAULT,'${content}','${imageUrl}','${visibleIn}',DEFAULT,'${email}')`)
-
     await client.end();
 
     return Response.json(result);
